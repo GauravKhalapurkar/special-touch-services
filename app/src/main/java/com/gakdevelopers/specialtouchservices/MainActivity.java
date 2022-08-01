606fb2ef27110b60c49685809bde72ed9e037496
Gaurav K <gaurav.khalapurkar@gmail.com>
2022-08-01 20:38:18 +0530

    Validation improved to get the current slot.

--- app/src/main/java/com/gakdevelopers/specialtouchservices/MainActivity.java
+++ app/src/main/java/com/gakdevelopers/specialtouchservices/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gakdevelopers.specialtouchservices;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,6 +29,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -36,9 +39,11 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtTherapistName, txtSlot;
+    TextView txtTherapistName, txtSlot, txtClientName;
 
-    ArrayList<String> listTherapists, listTime;
+    ArrayList<String> listTherapists, listTime, listClients, listDayOfWeek;
+
+    CardView cardHere, cardAbsent, cardPhoneCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +52,39 @@ public class MainActivity extends AppCompatActivity {
 
         txtTherapistName = (TextView) findViewById(R.id.txtTherapistName);
         txtSlot = (TextView) findViewById(R.id.txtSlot);
+        txtClientName = (TextView) findViewById(R.id.txtClientName);
 
-        /*String therapistName = getIntent().getStringExtra("therapistName");
-        txtTherapistName.setText("" + therapistName);*/
+        cardHere = (CardView) findViewById(R.id.cardHere);
+        cardAbsent = (CardView) findViewById(R.id.cardAbsent);
+        cardPhoneCall = (CardView) findViewById(R.id.cardPhoneCall);
 
         listTherapists = new ArrayList<>();
         listTime = new ArrayList<>();
+        listClients = new ArrayList<>();
+        listDayOfWeek = new ArrayList<>();
 
         getScheduleDetails();
+
+        cardHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardAbsent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardPhoneCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 
@@ -85,8 +115,10 @@ public class MainActivity extends AppCompatActivity {
         queue.add(stringRequest);
     }
 
+    @SuppressLint("DefaultLocale")
     private void parseItems(String jsonResponse) {
         String currentTimeSlot = "";
+        String accountHolder = txtTherapistName.getText().toString();
 
         ArrayList<HashMap<String, String>> list = new ArrayList<>();
 
@@ -108,34 +140,44 @@ public class MainActivity extends AppCompatActivity {
                 long total = minutesToMs + hoursToMs;
 
                 listTherapists.add(therapists);
+                listDayOfWeek.add(dayOfWeek);
                 listTime.add("" + total);
+                listClients.add(client);
             }
 
             String currentTime = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date());
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+            Date d = new Date();
+            String dayOfTheWeek = sdf.format(d);
 
             String[] tokens = currentTime.split(":");
             int minutesToMs = Integer.parseInt(tokens[1]) * 60000;
             int hoursToMs = Integer.parseInt(tokens[0]) * 3600000;
             long currentTimeInMS = minutesToMs + hoursToMs;
 
-            /*for(int i = 0; i<listTime.size(); i++) {
-                Log.d("ALL_TIMES", String.valueOf(Integer.parseInt(listTime.get(i))));
-            }*/
-
             for (int i = 0; i < listTime.size(); i++) {
-                if ((Integer.parseInt(String.valueOf(currentTimeInMS)) >= Integer.parseInt(listTime.get(i))) && (Integer.parseInt(String.valueOf(currentTimeInMS)) < Integer.parseInt(listTime.get(i+1))) ) {
+                if ( (accountHolder.equals(listTherapists.get(i)) && (Integer.parseInt(String.valueOf(currentTimeInMS)) >= Integer.parseInt(listTime.get(i)))) && dayOfTheWeek.equals(listDayOfWeek.get(i)) ) {
 
                     currentTimeSlot = String.format("%02d:%02d", TimeUnit.MILLISECONDS.toHours(Long.parseLong(listTime.get(i))),
                             TimeUnit.MILLISECONDS.toMinutes(Long.parseLong(listTime.get(i))) % TimeUnit.HOURS.toMinutes(1));
+
+                    txtSlot.setText(currentTimeSlot);
+                    txtClientName.setText(listClients.get(i));
 
                     //Log.d("FINAL_TIME", currentTimeSlot);
                 }
             }
 
-            txtSlot.setText(currentTimeSlot);
+            if (txtSlot.getText().toString().equals("Loading...")) {
+                txtSlot.setText("No bookings for now.");
+                txtClientName.setText("-");
+            }
 
-            //Log.d("THERAPIST_NAMES", String.valueOf(listTherapists));
-            //Log.d("LIST_TIME", String.valueOf(listTime));
+            //txtTherapistName.setText(currentTherapistName);
+
+
+            Log.d("THERAPIST_NAMES", String.valueOf(listTherapists));
+            Log.d("LIST_TIME", String.valueOf(listTime));
             //Log.d("CURRENT_TIME", String.valueOf(currentTime));
             //Log.d("TOTAL", String.valueOf(currentTimeInMS));
 
